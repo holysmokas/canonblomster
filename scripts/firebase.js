8cdd4a825b83b44af2396bd2c22aee794ac32937
holysmokas <holysmokasthatscheap@gmail.com>
2025-10-21 17:23:44 +0200

    changed the database handling paid to free

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -2,6 +2,8 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
     apiKey: "AIzaSyAZWzsKzT8Fc-_aI9gbWhZogrvmAmNs8Yg",
     authDomain: "canon-blomster.firebaseapp.com",
